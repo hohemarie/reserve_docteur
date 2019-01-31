65e3fa56ff87d24e550f39b1738c5512afc7fcea
marie goudon <marie.goudon@laposte.net>
2019-01-31 21:40:45 +0100

    ajout des fichiers

--- doc/db/seeds.rb
+++ doc/db/seeds.rb
@@ -1,6 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+
+require 'faker'
+
+100.times do
+  user = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: Faker::Industry.super_sector, zip_code: Faker::Address.zip_code) 
+end
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
